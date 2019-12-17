03270931fbf418adca1cd269647a6f6ee8fa0a2e
Cedric Von Felten <cedric.vonfelten@lamache.org>
2019-12-18 00:55:19 +0100

    formulaires

---
+++ resources/views/419.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::minimal')
+
+@section('title', __('Page Expired'))
+La requête n'a pas été authentifiée contre le CSRF !
+@section('code', '419')
+@section('message', __('Page Expired'))
